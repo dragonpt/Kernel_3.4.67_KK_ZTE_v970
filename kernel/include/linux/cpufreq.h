109f6621249085d10193f746ed19e058d1b2e8a1
dragonpt <cesar.maximo@gmail.com>
2014-12-10 23:25:44 +0000

    CPU GOVs: Added SmartAssV2 and PegasusQ

--- kernel/include/linux/cpufreq.h
+++ kernel/include/linux/cpufreq.h
@@ -364,6 +364,12 @@ extern struct cpufreq_governor cpufreq_gov_ondemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYBRID)
 extern struct cpufreq_governor cpufreq_gov_hybrid;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hybrid)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR (&amp;cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
+extern struct cpufreq_governor cpufreq_gov_pegasusq;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_pegasusq)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
